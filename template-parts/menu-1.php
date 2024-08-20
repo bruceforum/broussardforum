d101016bb7e47dcc4c84d1da4d6f747b76de2361
Michael Wrock <MichaelA.Wrock@gmail.com>
2024-08-20 14:33:00 -0400

    fix(header): remove menu wrapper

--- template-parts/menu-1.php
+++ template-parts/menu-1.php
@@ -10,18 +10,16 @@
 ?>
 
 <nav id="site-navigation" class="menu-1">
-	<div class="wrapper">
-		<button class="menu-toggle" aria-controls="site-menu" aria-expanded="false">
-			<?php esc_html_e( 'Site Navigation', 'scaffold' ); ?>
-		</button>
+	<button class="menu-toggle" aria-controls="site-menu" aria-expanded="false">
+		<?php esc_html_e( 'Site Navigation', 'scaffold' ); ?>
+	</button>
 
-		<?php
-		wp_nav_menu(
-			array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'site-menu',
-			)
-		);
-		?>
-	</div><!-- .wrapper -->
+	<?php
+	wp_nav_menu(
+		array(
+			'theme_location' => 'menu-1',
+			'menu_id'        => 'site-menu',
+		)
+	);
+	?>
 </nav><!-- .menu-1 -->
