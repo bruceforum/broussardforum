76e81010b8113e631815243799ed74279ad7594e
Michael Wrock <MichaelA.Wrock@gmail.com>
2024-09-26 19:39:09 -0400

    style: change menu rendering

--- template-parts/menu-1.php
+++ template-parts/menu-1.php
@@ -9,11 +9,10 @@
 
 ?>
 
+<button class="menu-toggle" aria-controls="site-menu" aria-expanded="false">
+	<?php esc_html_e( 'Site Navigation', 'scaffold' ); ?>
+</button>
 <nav id="site-navigation" class="menu-1">
-	<button class="menu-toggle" aria-controls="site-menu" aria-expanded="false">
-		<?php esc_html_e( 'Site Navigation', 'scaffold' ); ?>
-	</button>
-
 	<?php
 	wp_nav_menu(
 		array(
