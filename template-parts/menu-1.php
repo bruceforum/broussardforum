be760d36f6095d13000580abe03709a8b1dcbac5
Michael Wrock <MichaelA.Wrock@gmail.com>
2024-09-26 19:53:35 -0400

    style: move mobile menu

--- template-parts/menu-1.php
+++ template-parts/menu-1.php
@@ -9,10 +9,11 @@
 
 ?>
 
-<button class="menu-toggle" aria-controls="site-menu" aria-expanded="false">
-	<?php esc_html_e( 'Site Navigation', 'scaffold' ); ?>
-</button>
 <nav id="site-navigation" class="menu-1">
+	<button class="menu-toggle" aria-controls="site-menu" aria-expanded="false">
+		<?php esc_html_e( 'Site Navigation', 'scaffold' ); ?>
+	</button>
+
 	<?php
 	wp_nav_menu(
 		array(
